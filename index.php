4e20186cd797cd788a0073ce1f40d7d78c0ca015
Lisandro Zapata <zlisandro5@gmail.com>
2026-05-08 20:37:29 -0500

    estructura inicial del proyecto

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/banner.css">
+    <link rel="stylesheet" href="css/filters.css">
+    <link rel="stylesheet" href="css/week.css">
+    <link rel="stylesheet" href="css/navbar.css">
+    <title>Course Savio</title>
+</head>
+<body>
+    <?php
+    include "modules/navbar.php";
+    include "modules/banner.php";
+    include "modules/filters.php";
+    include "modules/week.php";
+    ?>
+</body>
+</html>
